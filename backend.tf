0c39dd0f94306d07a02af2e11995be2e36e9ba9e
Felipe Savoia <fsavoia@amazon.com>
2022-05-12 08:32:53 -0300

    update backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,11 @@
+##########################################################################################
+# Please, create your own tfstate backend bucket and dynamodb_table
+##########################################################################################
 terraform {
   backend "s3" {
-    bucket         = "tfsate-backend-poc-05092022"
-    key            = "poc.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
+    bucket         = "tfsate-backend-poc-05102022" # replace with your configuration
+    key            = "poc_aws_ecs.tfstate"
+    region         = "sa-east-1" # replace with your configuration
+    dynamodb_table = "terraform-state-lock" # replace with your configuration
   }
 }
